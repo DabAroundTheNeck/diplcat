63ca870bc66339e2bff5ffca6f1cd1656142e436
Michi2806 <michi2806@gmail.com>
2017-12-05 10:56:37 +0100

    PHPs

---
+++ php/function.php
@@ -0,0 +1,6 @@
+<?php
+    //Creating a PDO Connection
+    function create_pdo() {
+        return new PDO('mysql:host=localhost;dbname=d8abase;charset=utf8', 'root', 'HWSRootPass');
+    }
+?>
---
+++ php/login.php
@@ -0,0 +1,66 @@
+<?php
+  session_start();
+  include 'function.php';
+
+  //Creating a connection to the Database
+  $pdo = create_pdo();
+
+  echo "He";
+
+  try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->beginTransaction();
+
+    //Getting the login-data form the post
+    $post_data = file_get_contents("php://input");
+    $post_username = json_decode($post_data)->{'u'};
+    $post_password = json_decode($post_data)->{'pw'};
+
+    //Prepared Statement for the SQL procedure
+    $get_user_stmt = $pdo->prepare("call get_user_username(:username)");
+    $get_user_stmt->bindParam(':username', $post_username);
+
+    $get_user_stmt->execute();
+    $userdata = $get_user_stmt->fetch();
+
+    $get_user_stmt->closeCursor();
+
+    //Password verification
+    if ($userdata['iduser'] != "" && password_verify($post_password, $userdata['password'])) {
+      $session_id = session_id();
+      //Checking of user is already logged in
+      if (isset($userdata['session_id']) && $userdata['session_id'] != $session_id) {
+        session_write_close();
+        session_id($userdata['session_id']);
+        session_start();
+        session_unset();
+        session_destroy();
+        session_id($session_id);
+        session_start();
+      }
+      //Setting Session varibles
+      $_SESSION['userid'] = $userdata['iduser'];
+      $_SESSION['login'] = 1;
+      setcookie("cookiezi", $_SESSION['login'], 0, "/HWS");
+      //Update the stored Session id
+      $update_user_stmt = $pdo->prepare("call update_user_session_id(:id_in, :session_id_in)");
+      $update_user_stmt->bindParam(':id_in', $userdata['iduser']);
+      $update_user_stmt->bindParam(':session_id_in', $session_id);
+
+      $update_user_stmt->execute();
+      $update_user_stmt->closeCursor();
+      //Return value on Success
+      $response = array('response' => SUCCESS);
+    } else {
+      //Return value on false password verification
+      $response = array('response' => FAIL);
+    }
+    $pdo->commit();
+  } catch (Exception $e) {
+    //On SQL Error
+    $pdo->rollBack();
+    $response = array('response' => SQL_FAIL);
+  }
+  //Sending the return value to the js
+  echo json_encode($response);
+?>
