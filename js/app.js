b5a3b07d6a3aac42ca444703b6107b3b4a31a2d3
Michi2806 <michi2806@gmail.com>
2017-12-05 11:18:13 +0100

    Michl hat a bits gfeils

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function login() {
+
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+
+
+    var loginRequest = new XMLHttpRequest();
+    loginRequest.open('GET', './php/login.php');
+    loginRequest.send("e=email&pw=password");
+    loginRequest.onreadystatechange = function () {
+        if (loginRequest.readyState === DONE) {
+            if (loginRequest.status === OK) {
+                console.log(loginRequest.responseText);
+                let parsedResponse = JSON.parse(loginRequest.responseText);
+                console.log(parsedResponse);
+            } else {
+                console.log('Error: ' + loginRequest.status); // An error occurred during the request.
+            }
+        }
+    };
+}
---
+++ php/createUser.php
@@ -0,0 +1,48 @@
+<?php
+  session_start();
+  include 'function.php';
+
+  //Creating a connection to the Database
+  $pdo = create_pdo();
+
+  try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->beginTransaction();
+
+    //Getting the login-data form the post
+    $post_data = file_get_contents("php://input");
+    $post_email = json_decode($post_data)->{'e'};
+    $post_password = json_decode($post_data)->{'pw'};
+
+    //Prepared Statement for the SQL procedure
+    $get_userCount_stmt = $pdo->prepare("select count(*) as Count from users where email = :email");
+    $get_userCount_stmt->bindParam(':email', $post_email);
+
+    $get_userCount_stmt->execute();
+    $userCount = $get_userCount_stmt->fetch();
+
+    $get_userCount_stmt->closeCursor();
+
+    if ($get_userCount_stmt['Count'] <=0) {
+        $insert_user_stmt = $pdo->prepare("insert into users(email, passhash) values(:email, :passhash)");
+        $insert_user_stmt->bindParam(':email', $post_email);
+        $insert_user_stmt->bindParam(':passhash', password_hash($post_password, PASSWORD_DEFAULT));
+
+        $session_id = session_id();
+        $_SESSION['email'] = $userdata['email'];
+        $_SESSION['login'] = 1;
+        setcookie("cookiezi", $_SESSION['login'], 0, "/diplcat");
+
+        $response = array('response' => 'There was no Error');
+    } else {
+        $response = array('response' => 'This email is already in use');
+    }
+    $pdo->commit();
+  } catch (Exception $e) {
+    //On SQL Error
+    $pdo->rollBack();
+    $response = array('response' => 'There was an error with the SQL request');
+  }
+  //Sending the return value to the js
+  echo json_encode($response);
+?>
