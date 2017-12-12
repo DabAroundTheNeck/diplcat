eb19a7a5ad8865288451dab0d0a9780d0680f2fe
Michi2806 <michi2806@gmail.com>
2017-12-12 13:20:10 +0100

    Whyyyy Windows

--- csv/script.php
+++ csv/script.php
@@ -4,7 +4,7 @@
     for ($i=0; $i < count($users); $i++) {
         //echo $users[$i];
     }
-    echo $users[0];
+    //echo $users[0]['Email'];
 
     function csvToArray($filename) {
         $myfile = fopen($filename, "r") or die("Unable to open file!");
@@ -16,7 +16,18 @@
         }
         fclose($myfile);
         $headers = explode(";", $lines[0]);
+        $data = array();
 
-        return $headers;
+        echo $lines[count($lines)-1];
+
+        for ($i=1; $i < count($lines); $i++) {
+            $bits = explode(";", $lines[$i]);
+            $data[$i-1] = array();
+            for ($j=0; $j < count($headers); $j++) {
+                $data[$i-1][$headers[$j]] = $bits[$j];
+            }
+        }
+
+        return $data;
     }
 ?>
