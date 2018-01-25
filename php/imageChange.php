f7959047fe6ba51721949beb4c0019ea470175f0
michi2806 <michi2806@gmail.com>
2018-01-25 14:48:39 +0100

    Noice

--- php/imageChange.php
+++ php/imageChange.php
@@ -7,7 +7,7 @@
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        return $target_file;
+        return $_FILES;
         // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
