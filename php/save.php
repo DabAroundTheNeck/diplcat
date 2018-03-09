a2fc934f86b17c7c52bfc70de7f5a13b09ba2329
Michi2806 <michi2806@gmail.com>
2018-03-09 12:40:40 +0100

    Fehler behoben und text.txt file wird automatisch erstellt

--- php/save.php
+++ php/save.php
@@ -52,14 +52,30 @@
 
         $myfile = fopen($textfilename, 'a');
         fwrite($myfile, "\n\n");
-        fwrite($myfile, "Projektleiter\n\n");
-        fwrite($myfile, $filedata->projektleiter);
-        fwrite($myfile, "\n#Hier Bild vom Projektleiter " .$filedata->projektleiter->image. "\n\n");
+        fwrite($myfile, "Projektleiter:\n\n");
+        fwrite($myfile, $filedata->projektleiter->text . "\n");
+        fwrite($myfile, "#Hier Bild vom Projektleiter " .$filedata->projektleiter->image. "\n\n");
+        fwrite($myfile, "Mitarbeiter:\n\n");
         for ($i=0; $i < count($filedata->mitarbeiter); $i++) {
-            fwrite($myfile, "Mitarbeiter Nummer". ($i+1) . "\n\n");
+            fwrite($myfile, "Mitarbeiter ". ($i+1) . "\n");
             fwrite($myfile, $filedata->mitarbeiter[$i]->text . "\n");
             fwrite($myfile, "#Hier Bild des Projektmitarbeiter" . $filedata->mitarbeiter[$i]->image . "\n\n");
         }
+        fwrite($myfile, "Problemstellung:\n\n");
+        fwrite($myfile, $filedata->problemstellung . "\n\n");
+        fwrite($myfile, "Zielsetzung:\n\n");
+        fwrite($myfile, $filedata->zielsetzung . "\n\n");
+        fwrite($myfile, "Technologien:\n\n");
+        for ($i=0; $i < count($filedata->technologien); $i++) {
+            fwrite($myfile, "Technologie ". ($i+1) . "\n");
+            fwrite($myfile, $filedata->technologien[$i]->text . "\n");
+            fwrite($myfile, "#Hier Bild der Technologie" . $filedata->technologien[$i]->image . "\n\n");
+        }
+        fwrite($myfile, "Prototype:\n\n");
+        fwrite($myfile, $filedata->prototype->text . "\n");
+        fwrite($myfile, "#Hier Bild des Prototypen " .$filedata->prototype->image. "\n\n");
+        fwrite($myfile, "Ergebnisse:\n\n");
+        fwrite($myfile, $filedata->ergebnisse . "\n\n");
         fclose($myfile);
 
         $response = array('response' => 'File was saved to' . $filename);
