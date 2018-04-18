209a088e175732d1109925a2257a000046c1e0a2
Michi2806 <michi2806@gmail.com>
2018-04-18 08:32:03 +0200

    Fixed Merge

--- js/choice.js
+++ js/choice.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-function setThemaCookie(themaId) {
-    setCookie("thema", themaId, 0);
-    window.location.pathname = "/user/diplom/main.html";
-}
-
-if (getCookie('cookiezi') == 1) {
-    var themaRequest = new XMLHttpRequest();
-    themaRequest.open('POST', './php/getThemas.php');
-    themaRequest.send();
-    themaRequest.onreadystatechange = function () {
-        if (themaRequest.readyState === DONE) {
-            if (themaRequest.status === OK) {
-                let parsedResponse = JSON.parse(themaRequest.responseText);
-                console.log(parsedResponse);
-
-                if (parsedResponse.themas != null) {
-
-                    for (var i = 0; i < parsedResponse.themas.length; i++) {
-                        document.getElementById('container').insertAdjacentHTML('beforeend', '<div><button class="btn btn-primary" onclick="setThemaCookie('+
-                                                                                                parsedResponse.themas[i].idthema+')">'+parsedResponse.themas[i].name+
-                                                                                                '</button></div>');
-                    }
-
-                }
-            } else {
-                console.log('Error: ' + loginRequest.status); // An error occurred during the request.
-            }
-        }
-    };
-} else {
-    window.location.pathname = "/user/diplom/index.html";
-}
-=======
 function setThemaCookie(themaId) {
     setCookie("betreuer", "yes", 0);
     setCookie("thema", themaId, 0);
@@ -50,7 +16,7 @@ if (getCookie('cookiezi') == 1) {
 
                 if (parsedResponse.themas != null) {
                     for (var i = 0; i < parsedResponse.themas.length; i++) {
-                        document.getElementById('container').insertAdjacentHTML('beforeend', '<div><button onclick="setThemaCookie('+
+                        document.getElementById('container').insertAdjacentHTML('beforeend', '<div><button class="btn btn-primary" onclick="setThemaCookie('+
                                                                                                 parsedResponse.themas[i].idthema+')">'+parsedResponse.themas[i].name+
                                                                                                 '</button></div>');
                     }
@@ -63,4 +29,3 @@ if (getCookie('cookiezi') == 1) {
 } else {
     window.location.pathname = "/user/diplom/index.html";
 }
->>>>>>> f6d292500d84e42a848486771036364ba5403620
--- js/login.js
+++ js/login.js
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
 function login(form) {
 
     var loginRequest = new XMLHttpRequest();
     loginRequest.open('POST', './php/login.php');
     loginRequest.send('{"e":"'+form.email.value+'","pw":"'+form.psw.value+'"}');
     loginRequest.onreadystatechange = function () {
-      /*
-       if (this.readyState < 4 ||  this.status != 200) {
-          console.log("Verbindungsfehler!!");
-          alert("Aktueller Zugangsfehler Email//Password!");
-       } else
-       */
-       if (loginRequest.readyState === DONE) {
+        if (loginRequest.readyState === DONE) {
             if (loginRequest.status === OK) {
                 let parsedResponse = JSON.parse(loginRequest.responseText);
                 if (parsedResponse.response == 1) {
                     if (form.email.value.split("@")[1] == "htldornbirn.at") {   //If Email Ends on @htldornbirn.at its a teacher -> relocate to choice.html
                         window.location.pathname = "/user/diplom/choice.html";
                     } else {
+                        setCookie("betreuer", 0, 0);
                         window.location.pathname = "/user/diplom/main.html";
                     }
                 } else {
@@ -53,55 +47,4 @@ function register() {
 
 deleteCookie('cookiezi');
 deleteCookie('thema');
-=======
-
-function login(form) {
-
-    var loginRequest = new XMLHttpRequest();
-    loginRequest.open('POST', './php/login.php');
-    loginRequest.send('{"e":"'+form.email.value+'","pw":"'+form.psw.value+'"}');
-    loginRequest.onreadystatechange = function () {
-        if (loginRequest.readyState === DONE) {
-            if (loginRequest.status === OK) {
-                let parsedResponse = JSON.parse(loginRequest.responseText);
-                if (parsedResponse.response == 1) {
-                    if (form.email.value.split("@")[1] == "htldornbirn.at") {   //If Email Ends on @htldornbirn.at its a teacher -> relocate to choice.html
-                        window.location.pathname = "/user/diplom/choice.html";
-                    } else {
-                        setCookie("betreuer", 0, 0);
-                        window.location.pathname = "/user/diplom/main.html";
-                    }
-                } else {
-                    console.log("Login Failed");
-                }
-            } else {
-                console.log('Error: ' + loginRequest.status); // An error occurred during the request.
-            }
-        }
-    };
-}
-
-
-
-//Function is not used (There is no Registration)
-function register() {
-
-    var loginRequest = new XMLHttpRequest();
-    loginRequest.open('POST', './php/createUser.php');
-    loginRequest.send('{"e":"email","pw":"password"}');
-    loginRequest.onreadystatechange = function () {
-        if (loginRequest.readyState === DONE) {
-            if (loginRequest.status === OK) {
-                let parsedResponse = JSON.parse(loginRequest.responseText);
-                console.log(parsedResponse);
-            } else {
-                console.log('Error: ' + loginRequest.status); // An error occurred during the request.
-            }
-        }
-    };
-}
-
-deleteCookie('cookiezi');
-deleteCookie('thema');
 deleteCookie('betreuer');
->>>>>>> f6d292500d84e42a848486771036364ba5403620
